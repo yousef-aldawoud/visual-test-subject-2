f412cf476ea39a2be9ff33d5e3e73d244a4b48ef
Yousef <yousef.l.t.aldawoud@gmail.com>
2019-07-30 22:48:20 +1200

    Made todo-list

---
+++ src/components/Item.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="item">
+        {{text}} <i @click="deleteItem"  class="delete-icon fa fa-trash-o" aria-hidden="true"></i>
+    </div>
+</template>
+<script>
+export default {
+    props:{
+        text:{type:String,defualt:""}
+    },
+    methods: {
+      deleteItem(){
+          this.$emit("delete");
+      }  
+    },
+}
+</script>
+<style scoped>
+.item{
+    font-size: 20pt;
+}
+.delete-icon{
+    color:red;
+    cursor: pointer;
+}
+
+</style>
---
+++ src/components/List.vue
@@ -0,0 +1,52 @@
+<template>
+    <div>
+        <h1>HhdawFAWCVUDWA</h1>
+        <input type="text" name="input" id="input" v-model="inputValue" ><span class="plus-icon" @click="addItem">+</span>
+        <div class="error">{{error}}</div>
+        <hr style="width:50%">
+        <Item v-on:delete="deleteItem(index)" v-for="( item,index) in items" :key="index" :text="item"></Item>
+    </div>
+</template>
+<script>
+import Item from "./Item";
+export default {
+    components:{
+        Item,
+    },
+    data(){
+        return{
+            error:"",
+            inputValue:"",
+            items:[
+                "Hello wolrd",
+                "lol"
+            ]
+        }
+    },methods:{
+        deleteItem(index){
+            this.items.splice(index, 1);
+        },
+        addItem(){
+            if(this.inputValue ===""){
+                this.error = "WADcdgjihseofvEWADwau";
+                return;
+            }
+            this.items.push(this.inputValue);
+            this.inputValue = "";
+            this.error = "";
+        }
+    }
+}
+</script>
+<style scoped>
+.error{
+    color:red;
+    margin: 20px;
+}
+.plus-icon{
+    margin-left:5px; 
+    color:green;
+    font-size: 20pt;
+    cursor: pointer;
+}
+</style>
